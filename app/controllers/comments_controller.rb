983dfedbd7fe33a0f6d28227fb92e3c500d86e83
DCTorres87 <cruz_danielle_torres@yahoo.com>
2020-11-05 21:12:10 -0500

    modify the create method for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,10 +6,9 @@ class CommentsController < ApplicationController
     end
     
     def create
-        element = Element.find_by(id: params[:element_id]);
+
         comment = Comment.create(element_id: params[:element_id], name: params[:name], comment: params[:comment]);
+
         render :json => comment
     end 
-
-
 end
